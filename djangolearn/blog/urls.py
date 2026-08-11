3b54d061e82e74d4ebac9a63ac925944c724d992
Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2026-08-11 10:10:49 +0500

    completed URL parameters

--- djangolearn/blog/urls.py
+++ djangolearn/blog/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('post/<int:post_id>/', views.post_detial, name='post-detail'),
+    path('user/<str:username>/', views.user_profile, name='user-profile'),
+    path('article/<int:year>/<int:month>/<int:day>/', views.article_details, name="article-detail"),
+    
+    re_path(r'^article/(?P<year>[0-9]{4})/$', views.article_by_year, name='article-by-year')
 ]
--- djangolearn/blog/views.py
+++ djangolearn/blog/views.py
@@ -8,3 +8,15 @@ def home(request):
 
 def about(request):
     return HttpResponse(f"Welcome to Blog about page")
+
+def post_detial(request, post_id):
+    return HttpResponse(f"Show blog post {post_id}")
+
+def user_profile(request, username):
+    return HttpResponse(f"Profile of User {username}")
+
+def article_by_year(reqeust, year):
+    return HttpResponse(f"Articles of year {year}:")
+
+def article_details(reqeust, **kwargs):
+    return HttpResponse(f"Details:  {kwargs}")
