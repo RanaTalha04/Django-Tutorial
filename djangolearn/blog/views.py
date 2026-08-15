4087bf9b339cce3968463dd353ad8ec955908421
Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2026-08-15 11:45:13 +0500

    Template # 1, Basics variables

--- djangolearn/blog/views.py
+++ djangolearn/blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
+
+class User: 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+def home(request):
+    context = {
+        "name": "Muhammad Talha",
+        "age": 22,
+        "skills": ["Python", "Gen AI", "SQA"],
+        "user": User("Muhammad Ali", 24),
+        "blog": {
+            "title": "First Django Blog",
+            "content": "This is the first django blog",
+            "created_at": datetime(2026, 8, 15, 11, 5)
+        },
+        "empty_value": None,
+        "bold_value": "<b>This is bold value</b>"
+    }
+    return render(request, "blog/home.html", context)
+    
+
 
 def about(request):
     return HttpResponse(f"Welcome to Blog about page")
-
-def home(request):
-    return render(request, "blog/post_list.html")
